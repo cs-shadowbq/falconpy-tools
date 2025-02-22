3181c70a86c73174437fd25d7d17fed49341b841
shadowbq <shadowbq@gmail.com>
2025-02-22 15:59:38 -0500

    updated

--- bin/falcon_validate_creds_shell.py
+++ bin/falcon_validate_creds_shell.py
@@ -1,4 +1,6 @@
 # flake8: noqa=W605  pylint: disable=W1401
+import code
+from contextlib import suppress
 import sys
 from argparse import ArgumentParser, RawTextHelpFormatter
 try:
@@ -17,17 +19,13 @@ parser.add_argument('-k', '--key', help="CrowdStrike API Key", required=True)
 parser.add_argument('-s', '--secret', help="CrowdStrike API Secret", required=True)
 parser.add_argument('-x', '--skip-ssl', help="Skip SSL verification", required=False, action='store_true')
 parser.add_argument('-b', '--base_url', help="CrowdStrike base URL for Gov Clouds", required=False, default="auto")
-
-# add verbose options
-parser.add_argument('-v', '--verbose', help="Enable verbose mode", required=False, action='store_true')
-
 args = parser.parse_args()
 CLIENT_ID = args.key
 CLIENT_SECRET = args.secret
-CLIENT_BASE = args.base_url
+CLIENT_BASE = args.base
 
 # Login to the Falcon API and retrieve our list of sensors
-if args.base_url == "auto":
+if args.base == "auto":
     if args.skip_ssl:
         falcon = APIHarness(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, ssl_verify=False)
     else:
@@ -38,11 +36,10 @@ else:
     else:
         falcon = APIHarness(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, base_url=CLIENT_BASE)
 
+with suppress(SystemExit):
+    code.interact(local=dict(globals(), **locals()))
+
 if falcon.authenticate():
-    if args.verbose:
-        print("Authentication successful")
     sys.exit(0)
 else:
-    if args.verbose:
-        print("Authentication failed")
     sys.exit(1)
